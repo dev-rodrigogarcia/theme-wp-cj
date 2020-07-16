85438ef37c8532063d21eb3054ec3a127e475ae7
rodrigo.santos <rodrigo.santos@sensedia.com>
2020-07-16 20:24:48 -0300

    Ajustes da tela de single job

--- page-vaga.php
+++ page-vaga.php
@@ -23,7 +23,7 @@
 
             <div class="pl-3 pr-3">
                <div class="row">
-                  <div class="col">
+                  <div class="col-sm-12 col-md-8 col-lg-9 col-xl-9">
                      <h1 class="single-job-title">
                         Desenvolvedor React Jr
                      </h1>
@@ -33,7 +33,7 @@
                         Campinas - SP
                      </h2>
       
-                     <div class="tags d-flex flex-wrap justify-content-start mt-3">
+                     <div class="tags d-flex flex-wrap justify-content-center justify-content-sm-start mt-3">
                         <p class="job-tag job-pcd d-flex align-items-center justify-content-center">
                            <em class="fas fa-wheelchair"></em>
                            PCD
@@ -51,8 +51,9 @@
                      </div>
                   </div>
    
-                  <div class="col">
-                     <div class="action-button-social d-flex justify-content-end">
+                  <div class="col-sm-12 col-md-4 col-lg-3 col-xl-3">
+                     <div class="mb-3 d-block .d-sm-none"></div>
+                     <div class="action-button-social d-flex justify-content-center">
                         <a href="#" class="apply-facebook">
                            <em class="fab fa-facebook-f"></em>
                         </a>
@@ -70,65 +71,127 @@
       </div>
 
       <div class="container mb-5">
-         <div class="row">
+         <div class="row job-body p-4 pt-5 mr-0 ml-0">
             <div class="col-md-8">
-               <div class="job-body p-4">
-                  <div class="row p-3">
-                     <div class="col">
-                        <h5 class="job-title-subhead mb-3">
-                           Descrição
-                        </h5>
-      
-                        <p class="mb-5">
-                           Lorem ipsum dolor sit amet consectetur adipiscing, elit vehicula semper velit vestibulum felis purus, gravida rhoncus vulputate aliquet cras. Conubia libero morbi tristique rutrum elementum dapibus per cras volutpat, semper consequat nisl aenean urna ultricies tincidunt etiam senectus. Rhoncus blandit neque vivamus nullam sodales maecenas felis faucibus, lectus suspendisse vitae donec hendrerit montes ultrices fames, penatibus est pulvinar sagittis proin phareultrices fringilla.
-                        </p>
-      
-                        <h5 class="job-title-subhead mb-3">
-                           Responsabilidades
-                        </h5>
-      
-                        <p class="mb-5">
-                           Lorem ipsum dolor sit amet consectetur adipiscing, elit vehicula semper velit vestibulum felis purus, gravida rhoncus vulputate aliquet cras. Conubia libero morbi tristique rutrum elementum dapibus per cras volutpat, semper consequat nisl aenean urna ultricies tincidunt etiam senectus. Rhoncus blandit neque vivamus nullam sodales maecenas felis faucibus, lectus suspendisse vitae donec hendrerit montes ultrices fames, penatibus est pulvinar sagittis proin phareultrices fringilla.
-                        </p>
-      
-                        <h5 class="job-title-subhead mb-3">
-                           Benefícios
-                        </h5>
-      
-                        <p class="mb-5">
-                           Lorem ipsum dolor sit amet consectetur adipiscing, elit vehicula semper velit vestibulum felis purus, gravida rhoncus vulputate aliquet cras. Conubia libero morbi tristique rutrum elementum dapibus per cras volutpat, semper consequat nisl aenean urna ultricies tincidunt etiam senectus. Rhoncus blandit neque vivamus nullam sodales maecenas felis faucibus, lectus suspendisse vitae donec hendrerit montes ultrices fames, penatibus est pulvinar sagittis proin phareultrices fringilla.
-                        </p>
-      
-                        <h5 class="job-title-subhead mb-3">
-                           Observação
-                        </h5>
-      
-                        <p class="mb-5">
-                           Lorem ipsum dolor sit amet consectetur adipiscing, elit vehicula semper velit vestibulum felis purus, gravida rhoncus vulputate aliquet cras. Conubia libero morbi tristique rutrum elementum dapibus per cras volutpat, semper consequat nisl aenean urna ultricies tincidunt etiam senectus. Rhoncus blandit neque vivamus nullam sodales maecenas felis faucibus, lectus suspendisse vitae donec hendrerit montes ultrices fames, penatibus est pulvinar sagittis proin phareultrices fringilla.
-                        </p>
-      
-                        <h5 class="job-title-subhead mb-3">
-                           Como participar
-                        </h5>
-      
-                        <p class="mb-5">
-                           Lorem ipsum dolor sit amet consectetur adipiscing, elit vehicula semper velit vestibulum felis purus, gravida rhoncus vulputate aliquet cras. Conubia libero morbi tristique rutrum elementum dapibus per cras volutpat, semper consequat nisl aenean urna ultricies tincidunt etiam senectus. Rhoncus blandit neque vivamus nullam sodales maecenas felis faucibus, lectus suspendisse vitae donec hendrerit montes ultrices fames, penatibus est pulvinar sagittis proin phareultrices fringilla.
-                        </p>
-      
+               <div class="row">
+                  <div class="col">
+                     <h5 class="job-title-subhead mb-3">
+                        Descrição
+                     </h5>
+   
+                     <p class="mb-5 text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipiscing, elit vehicula semper velit vestibulum felis purus, gravida rhoncus vulputate aliquet cras. Conubia libero morbi tristique rutrum elementum dapibus per cras volutpat, semper consequat nisl aenean urna ultricies tincidunt etiam senectus. Rhoncus blandit neque vivamus nullam sodales maecenas felis faucibus, lectus suspendisse vitae donec hendrerit montes ultrices fames, penatibus est pulvinar sagittis proin phareultrices fringilla.
+                     </p>
+   
+                     <h5 class="job-title-subhead mb-3">
+                        Benefícios
+                     </h5>
+   
+                     <div class="benefits d-flex flex-wrap justify-content-between">
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-bus-alt"></em>
+                           Vale Transporte
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-utensils"></em>
+                           Refeição no local
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-gas-pump"></em>
+                           Ajuda com Combustível
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-dumbbell"></em>
+                           Academia
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-tooth"></em>
+                           Plano Odontológico
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-ambulance"></em>
+                           Plano Médico
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-graduation-cap"></em>
+                           Ajuda com Estudos
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-laptop-house"></em>
+                           Trabalho Remoto
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-comment-dollar"></em>
+                           Comissão
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-hand-holding-usd"></em>
+                           Participação no Lucros
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-handshake"></em>
+                           Desconto em Parceiros
+                        </div>
+
+                        <div class="icon-box p-3 d-flex align-items-center">
+                           <em class="fas fa-cart-plus"></em>
+                           Vale Alimentação
+                        </div>
                      </div>
+
+                     <p class="mt-4 mb-5 text-justify">                        
+                        Mais alguns:
+                        dsdsd
+                     </p>
+   
+                     <h5 class="job-title-subhead mb-3">
+                        Observação
+                     </h5>
+   
+                     <p class="mb-5 text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipiscing, elit vehicula semper velit vestibulum felis purus, gravida rhoncus vulputate aliquet cras. Conubia libero morbi tristique rutrum elementum dapibus per cras volutpat, semper consequat nisl aenean urna ultricies tincidunt etiam senectus. Rhoncus blandit neque vivamus nullam sodales maecenas felis faucibus, lectus suspendisse vitae donec hendrerit montes ultrices fames, penatibus est pulvinar sagittis proin phareultrices fringilla.
+                     </p>
+   
+                     <h5 class="job-title-subhead mb-3">
+                        Como participar
+                     </h5>
+
+                     <div class="alert alert-info" role="alert">
+                        <strong>PSIU!</strong>
+                        <br/>
+                        <p class="text-justify mt-2">
+                           As vagas disponibilizadas aqui são gratuitas, não pague ou compartilhe dados suspeitos com ninguém!
+                        </p>
+                     </div>
+   
+                     <br/>
+
+                     <p class="mb-5 text-justify">
+                        Os interessados deverão encaminhar o currículo aos cuidados de <strong>Priscila</strong> para o e-mail <strong>priscila.selecao2018@gmail.com</strong>
+                        com a sigla <strong>Nutricionista</strong> no campo assunto até o dia <strong>20/07/2020</strong>.
+                     </p>
+   
                   </div>
                </div>
             </div>
 
             <div class="col-md-4">
-               <div class="job-body p-4">
-                  dsdsdsd<br/>
-                  dsdsdsd<br/>
-                  dsdsdsd<br/>
-                  dsdsdsd<br/>
-                  dsdsdsd<br/>
-                  dsdsdsd<br/>
-               </div>
+               dsdsdsd<br/>
+               dsdsdsd<br/>
+               dsdsdsd<br/>
+               dsdsdsd<br/>
+               dsdsdsd<br/>
+               dsdsdsd<br/>
             </div>
          </div>
       </div>
